04b5da7a0b1626324f69bbe58686c47514dd1941
Gabriella Ruiz <gabriella.ruiz@go.mnstate.edu>
2022-01-20 12:52:21 -0600

    Completed lab 2

--- assignment.R
+++ assignment.R
@@ -1,8 +1,33 @@
 library(tidyverse)
 library(palmerpenguins)
+library(ggplot2)
+
+Question 10C
 
 ggplot(data = penguins) + 
-  geom_point(mapping = aes(x = bodymass, y = flipperlength))
+  geom_point(mapping = aes(x = body_mass_g, y = flipper_length_mm))
+
+Question 11B
 
 ggplot(data = penguins) + 
-  <GEOM_FUNCTION>(mapping = aes(<MAPPINGS>))
+  geom_point(mapping = aes(x = flipper_length_mm, 
+                           y = body_mass_g, 
+                           color = species,
+                           shape = island))
+Question 13B
+
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = flipper_length_mm,
+                           y = body_mass_g,
+                           color = species,
+                           shape = island)) + 
+  facet_wrap(~ species, nrow = 1)
+
+Question 13C
+
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = flipper_length_mm,
+                           y = body_mass_g,
+                           color = species,
+                           shape = island)) + 
+  facet_grid(species ~ island)
